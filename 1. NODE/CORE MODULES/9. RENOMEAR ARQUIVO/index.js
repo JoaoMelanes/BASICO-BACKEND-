ae1e311b0ebb3db0fc01b433e168eb119b406022
João Pedro <joaomelanes12@gmail.com>
2025-04-04 13:29:24 -0300

    Atualização

---
+++ 1. NODE/CORE MODULES/9. RENOMEAR ARQUIVO/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const arquivoAntigo = 'arquivo.txt'
+const arquivoNovo = 'novo.txt'
+
+fs.rename(arquivoAntigo, arquivoNovo, (err) => {
+    if(err){
+        console.log(err)
+        return
+    }
+
+    console.log(`O arquivo: ${arquivoAntigo} foi renomeado para: ${arquivoNovo}`)
+})
