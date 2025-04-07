d51fac0dcb6e13c17572aaf09231f6e405463659
João Pedro <joaomelanes12@gmail.com>
2025-04-07 12:07:52 -0300

    atualização

---
+++ 1. NODE/EXPRESS/10. 404/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+const user = require('./users')
+const app = express()
+const basePath = path.join(__dirname, 'templates')
+
+// arquivo estatico
+app.use(express.static('public'))
+
+// Ler body post
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.use('/users', user)
+
+const port = 3000 
+
+app.get('/',(req, res) => {
+
+    res.sendFile(`${basePath}/index.html`, (err) => {
+        console.log(err)
+    })
+})
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(`${basePath}/404.html`)
+})
+
+app.listen(port, () => {
+
+    console.log(`App rodando na porta: ${port}`)
+})
+
+
+
