6fffd11643adc422c3e52ce746953c082a492cd4
João Pedro <joaomelanes12@gmail.com>
2025-04-10 16:09:05 -0300

    Atualização

--- 1. NODE/SEQUELIZE/12. DELET RELACIONAIS/index.js
+++ 1. NODE/SEQUELIZE/12. DELET RELACIONAIS/index.js
@@ -124,6 +124,17 @@ app.post('/address/create', async (req, res) => {
 
 })
 
+app.post('/address/delete/', async (req, res) => {
+    const UserId = req.body.UserId
+    const id = req.body.id
+
+    await Address.destroy( {
+        where: {id : id}
+    })
+
+    res.redirect(`/users/edit/${UserId}`)
+})
+
 app.get('/', async (req, res) => {
 
     // raw trás um array só com os dados do banco de dados
