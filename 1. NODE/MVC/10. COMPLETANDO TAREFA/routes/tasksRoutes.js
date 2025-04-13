3b62104cc98b72efaf6150f3ffde7cc1bffe4aae
João Pedro <joaomelanes12@gmail.com>
2025-04-13 08:51:17 -0300

    Atualização

---
+++ 1. NODE/MVC/10. COMPLETANDO TAREFA/routes/tasksRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const TaskController = require('../controllers/TaskController')
+
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.deleteTask)
+router.get('/edit/:id', TaskController.taskEdit)
+router.post('/update', TaskController.taskUpdate)
+router.post('/updatestatus', TaskController.taggleTaskStatus)
+router.get('/', TaskController.showTasks)
+
+module.exports = router
