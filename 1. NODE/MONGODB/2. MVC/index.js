5a9c49b460c3c2031f41cc53de0ad2ed78f7714b
João Pedro <joaomelanes12@gmail.com>
2025-04-16 12:16:52 -0300

    Atualização

---
+++ 1. NODE/MONGODB/2. MVC/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+
+const productsRouter = require('./routes/productsRoutes')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+// read body
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use('/products', productsRouter)
+
+app.listen(3003)
