d509cd385701d47a201ced03c228919760f5c3b1
João Pedro <joaomelanes12@gmail.com>
2025-04-17 11:29:18 -0300

    Atualização

---
+++ 2. APIs/3. STATUS NO RES/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+// Rotas -- endpoints
+app.post('/createproduct', (req,res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    if(!name){
+        res.status(422).json({message: `O campo nome é obrigatorio`})
+        return
+    }
+    res.status(201).json({message: `O Produto ${name}, foi criado com sucesso!`})
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Primeira rota criada com sucesso!'})
+})
+
+app.listen(3003)
